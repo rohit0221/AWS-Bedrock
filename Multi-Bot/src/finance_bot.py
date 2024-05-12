76129869d4f47b0c1a7bdb127bf0bcb584f0efc3
rohit0221 <rohit0221@gmail.com>
2024-05-12 14:10:17 +0530

    .

--- Multi-Bot/src/finance_bot.py
+++ Multi-Bot/src/finance_bot.py
@@ -9,27 +9,27 @@ load_dotenv()
 from langchain_community.llms import Bedrock
 from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
 from utils import *
-def get_prompttemplate_codebot(code_suboption):
+def get_prompttemplate_financebot(code_suboption):
     if code_suboption =="Create Code Documentation":
-        with open("../prompts/code-bot/code-docu-prompt.txt", "r") as file:
+        with open("../prompts/finance-bot/profit-prompt.txt", "r") as file:
             prompt_template = file.read()
     elif code_suboption =="Add Comments to my Code":
-        with open("../prompts/code-bot/code-comment-prompt.txt", "r") as file:
+        with open("../prompts/finance-bot/revenue-prompt.txt", "r") as file:
             prompt_template = file.read()
     elif code_suboption =="Explain the Architecture of my Code":
-        with open("../prompts/code-bot/code-architecture-prompt.txt", "r") as file:
+        with open("../prompts/finance-bot/debt-prompt.txt", "r") as file:
             prompt_template = file.read() 
     return prompt_template            
 
-def get_conversational_chain_codebot(code_suboption):
-    prompt_template=get_prompttemplate_codebot(code_suboption)
+def get_conversational_chain_financebot(code_suboption):
+    prompt_template=get_prompttemplate_financebot(code_suboption)
     prompt = prepare_prompt_gen(prompt_template)
     model = get_model_gen()
     chain = LLMChain(llm=model, prompt=prompt)
     return chain
 
-def user_input_codebot(user_question,code_suboption):
-    chain = get_conversational_chain_codebot(code_suboption)
+def user_input_financebot(user_question,code_suboption):
+    chain = get_conversational_chain_financebot(code_suboption)
     response = chain(
         {"question": user_question}
         , return_only_outputs=True)
