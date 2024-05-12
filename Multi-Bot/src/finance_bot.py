604b2bfaccf428e285aec385ec6f94e4435e026f
rohit0221 <rohit0221@gmail.com>
2024-05-12 14:30:52 +0530

    .

--- Multi-Bot/src/finance_bot.py
+++ Multi-Bot/src/finance_bot.py
@@ -9,28 +9,36 @@ load_dotenv()
 from langchain_community.llms import Bedrock
 from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
 from utils import *
-def get_prompttemplate_financebot(code_suboption):
-    if code_suboption =="Create Code Documentation":
-        with open("../prompts/finance-bot/profit-prompt.txt", "r") as file:
-            prompt_template = file.read()
-    elif code_suboption =="Add Comments to my Code":
+
+
+def get_prompttemplate_financebot(suboption):
+    if suboption =="Revenue Numbers":
         with open("../prompts/finance-bot/revenue-prompt.txt", "r") as file:
             prompt_template = file.read()
-    elif code_suboption =="Explain the Architecture of my Code":
+    elif suboption =="Profit Numbers":
+        with open("../prompts/finance-bot/revenue-prompt.txt", "r") as file:
+            prompt_template = file.read()
+    elif suboption =="Debt Details":
         with open("../prompts/finance-bot/debt-prompt.txt", "r") as file:
             prompt_template = file.read() 
     return prompt_template            
 
-def get_conversational_chain_financebot(code_suboption):
-    prompt_template=get_prompttemplate_financebot(code_suboption)
+
+def get_conversational_chain_financebot(suboption):
+    prompt_template=get_prompttemplate_financebot(suboption)
     prompt = prepare_prompt_gen(prompt_template)
     model = get_model_gen()
-    chain = LLMChain(llm=model, prompt=prompt)
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
 
-def user_input_financebot(user_question,code_suboption):
-    chain = get_conversational_chain_financebot(code_suboption)
+def user_input_financebot(user_question,suboption):
+    knowledge_base="OMNDAYJNHH"
+    retriever = AmazonKnowledgeBasesRetriever(
+    knowledge_base_id=knowledge_base,
+    retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 4}},)
+    docs = retriever.invoke(user_question)   
+    chain = get_conversational_chain_financebot(suboption)
     response = chain(
-        {"question": user_question}
+        {"input_documents":docs, "question": user_question}
         , return_only_outputs=True)
     return response
